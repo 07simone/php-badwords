74ff6e0a3e81485ae385d19656bafa7876ed09b2
simone andreozzi <simoneandreozzi8@gmail.com>
2022-04-19 17:00:12 +0200

    inserimento-Get

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php 
 
     $paragrafo = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur tempore cumque vitae iusto excepturi in officiis illo unde, repudiandae dicta iure corporis, hic velit ipsa eveniet molestias odio modi? Explicabo!";
-
+    $parola = $_GET['paragrafo'];
+    echo (str_replace( $parola , "***", $paragrafo)); 
     
-    echo (str_replace( "sit", "***",$paragrafo)); 
-    echo " La lunghezza è " .strlen($paragrafo);
+    
+    
 ?>
 
+<pre>
+<?php 
+    
+    echo " La lunghezza è " . strlen($paragrafo);
+    
+?>
+</pre>

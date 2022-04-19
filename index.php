a539956896c5991ff94c27cf5c25042a072e9f6f
simone andreozzi <simoneandreozzi8@gmail.com>
2022-04-19 15:31:15 +0200

    esercizio-completato

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+
+    $paragrafo = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur tempore cumque vitae iusto excepturi in officiis illo unde, repudiandae dicta iure corporis, hic velit ipsa eveniet molestias odio modi? Explicabo!";
+
+    
+    echo (str_replace( "sit", "***",$paragrafo)); 
+    echo " La lunghezza è " .strlen($paragrafo);
+?>
+
